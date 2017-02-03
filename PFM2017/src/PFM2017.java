d8e2e0a2c4767bf81662b09712f0fecc2f2a2e0b
Jeroen Goedhart <j.goedhart@outlook.com>
2017-02-03 13:30:31 +0100

    Updated to assignment 1

--- PFM2017/src/PFM2017.java
+++ PFM2017/src/PFM2017.java
@@ -3,9 +3,21 @@ public class PFM2017 {
 
 	public static void main(String[] args) {
 		
-		// print hello
-		System.out.print("hello world");
+		double orderAmount = 30;
+		double shippingCosts = 0;
+		double totalAmount = 0;
 
+		if (orderAmount <= 50.00 ) {
+			shippingCosts = 8;
+			totalAmount = shippingCosts + orderAmount;
+		} else if (orderAmount > 50.00 && orderAmount <= 100.00) {
+			shippingCosts = 4;
+			totalAmount = shippingCosts + orderAmount;
+		} else {
+			totalAmount = orderAmount;
+		}
+		
+		String outputText = "The total payable amount is €" + totalAmount + ".";
+		System.out.print(outputText);
 	}
-
 }
